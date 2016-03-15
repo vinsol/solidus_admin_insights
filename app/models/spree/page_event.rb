0951dc8cca0039a9acb90c4b873f72b6d7ea7eac
jaspreet21anand <jaspreet21anand@gmail.com>
2016-03-15 18:16:32 +0530

    add validations in page_event model

--- app/models/spree/page_event.rb
+++ app/models/spree/page_event.rb
@@ -2,5 +2,7 @@ module Spree
   class PageEvent < Spree::Base
     belongs_to :actor, polymorphic: true
     belongs_to :object, polymorphic: true
+
+    validates :referrer, :session_id, :activity, presence: true
   end
 end

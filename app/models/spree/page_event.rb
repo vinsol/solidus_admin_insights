b0d7ec139ba640b366a21bfdbd6c9d8cedfab59a
jaspreet21anand <jaspreet21anand@gmail.com>
2016-03-17 17:31:00 +0530

    add report product_views_to_cart_additions

--- app/models/spree/page_event.rb
+++ app/models/spree/page_event.rb
@@ -9,5 +9,8 @@ module Spree
     validates :activity,
               :session_id, presence: true
 
+    scope :product_pages, -> { where(target_type: Spree::Product) }
+
+    self.whitelisted_ransackable_attributes = ['created_at']
   end
 end
--- app/services/spree/report_generation_service.rb
+++ app/services/spree/report_generation_service.rb
@@ -1,19 +1,30 @@
 module Spree
   class ReportGenerationService
     REPORTS = {
-      product_views: { headers: [:product_name, :view_count, :user_count, :guest_sessions] },
-      cart_additions: { headers: [:product_name, :addition_count, :quantity_change] },
-      cart_removals: { headers: [:product_name, :removal_count, :quantity_change] },
-      cart_updations: { headers: [:product_name, :updation_count, :quantity_increase, :quantity_decrease] }
+      product_views: {
+        headers: [:product_name, :views, :users, :guest_sessions]
+      },
+      cart_additions: {
+        headers: [:product_name, :additions, :quantity_change]
+      },
+      cart_removals: {
+        headers: [:product_name, :removals, :quantity_change]
+      },
+      cart_updations: {
+        headers: [:product_name, :updations, :quantity_increase, :quantity_decrease]
+      },
+      product_views_to_cart_additions: {
+        headers: [:product_name, :views, :cart_additions]
+      }
     }
 
     def self.product_views(options = {})
       product_view = Struct.new(*REPORTS[:product_views][:headers])
-      search = Spree::PageEvent.where(target_type: Spree::Product).ransack(options[:q])
+      search = Spree::PageEvent.product_pages.ransack(options[:q])
       product_views = search.result.group_by(&:target_id).map do |_id, page_events|
         view = product_view.new(Spree::Product.find_by(id: _id).name)
-        view.view_count = page_events.size
-        view.user_count = page_events.select(&:actor_id?).uniq(&:actor_id).size
+        view.views = page_events.size
+        view.users = page_events.select(&:actor_id?).uniq(&:actor_id).size
         view.guest_sessions = page_events.reject(&:actor_id?).uniq(&:session_id).size
         view
       end
@@ -33,12 +44,29 @@ module Spree
       search = Spree::CartEvent.events(:update).ransack(options[:q])
       cart_additions = search.result.group_by(&:product).map do |product, cart_events|
         view = cart_additions_view.new(product.name)
-        view.updation_count = cart_events.size
+        view.updations = cart_events.size
         view.quantity_increase = cart_events.map(&:quantity).select { |quantity| quantity > 0 }.sum
         view.quantity_decrease = cart_events.map(&:quantity).select { |quantity| quantity < 0 }.sum
         view
       end
       [search, cart_additions]
+    end
+
+    def self.product_views_to_cart_additions(options = {})
+      product_to_cart_view = Struct.new(*REPORTS[:product_views_to_cart_additions][:headers])
+      product_views_to_cart_additions = self.product_views(options).second.map do |product_view|
+        view = product_to_cart_view.new(product_view.product_name)
+        view.views = product_view.views
+        view
+      end
+
+      self.cart_additions(options).second.each do |cart_addition|
+        product_view_cart_addition = product_views_to_cart_additions.find(ifnone = product_to_cart_view.new(cart_addition.product_name, 0)) do |view|
+          view.product_name == cart_addition.product_name
+        end
+        product_view_cart_addition.cart_additions = cart_addition.additions
+      end
+      [self.product_views(options).first, product_views_to_cart_additions]
     end
 
     class << self

0f260fd9a405045b36d789886d4b6dbaf4ff4bb2
Tanmay Sinha <tanmay3011@gmail.com>
2016-03-21 16:03:37 +0530

    Add default value for orders_completed_at

--- app/controllers/spree/admin/insights_controller.rb
+++ app/controllers/spree/admin/insights_controller.rb
@@ -2,6 +2,7 @@ module Spree
   module Admin
     class InsightsController < Spree::Admin::BaseController
       before_action :ensure_report_exists, only: :show
+      before_action :set_default_completed_at, only: :show
       before_action :load_reports, only: :index
 
       def show
@@ -20,6 +21,17 @@ module Spree
 
         def load_reports
           @reports = ReportGenerationService::REPORTS.keys
+        end
+
+        def set_default_completed_at
+          params[:q] = {} unless params[:q]
+          if params[:q][:orders_created_at_gt].blank? && @report_name == :users_who_recently_purchased
+            params[:q][:orders_created_at_gt] = Date.current.beginning_of_month
+          end
+
+          if params[:q][:orders_completed_at_lt].blank? && @report_name == :users_who_have_not_purchased_recently
+            params[:q][:orders_completed_at_lt] = Date.current.end_of_month
+          end
         end
     end
   end

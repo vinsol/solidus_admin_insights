03fb2c544ef701105ffeb8915c7983d5bc7e174e
nimish <nimish.gupta@vinsol.com>
2016-04-05 18:45:10 +0530

    change page select-box padding

--- app/helpers/spree/admin/base_helper_decorator.rb
+++ app/helpers/spree/admin/base_helper_decorator.rb
@@ -6,4 +6,9 @@ Spree::BaseHelper.class_eval do
   def form_action(insight, insight_type)
     insight ? admin_insight_path(id: @report_name, type: insight_type) : 'javascript:void(0)'
   end
+
+  def page_selector_options
+    [5, 10, 15, 30, 45, 60]
+  end
+
 end

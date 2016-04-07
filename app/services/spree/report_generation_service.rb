241315e8176817416fc303d45cd17ceef598c5e9
Tanmay Sinha <tanmay3011@gmail.com>
2016-04-07 11:24:29 +0530

    Fix css for buttons

--- app/services/spree/report_generation_service.rb
+++ app/services/spree/report_generation_service.rb
@@ -27,7 +27,7 @@ module Spree
         dataset = resource.generate
         total_records = resource.select_columns(dataset).count
         result_set = resource.select_columns(dataset.limit(options['records_per_page'], options['offset'])).all
-        [headers(klass, resource), result_set, total_pages(total_records, options['records_per_page'], options['no_pagination']), search_attributes(klass)]
+        [headers(klass, resource, report_name), result_set, total_pages(total_records, options['records_per_page'], options['no_pagination']), search_attributes(klass)]
       else
         [headers(klass, options, report_name), sales_performance(options), total_pages(1, options['records_per_page']), search_attributes(klass)]
       end

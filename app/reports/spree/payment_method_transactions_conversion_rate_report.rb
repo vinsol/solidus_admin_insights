7b21a05804a254e51e3a85bf0ffee328f0387ee0
jaspreet21anand <jaspreet21anand@gmail.com>
2016-04-20 17:38:00 +0530

    payment method conversion rate report grouped by months

--- app/reports/spree/payment_method_transactions_conversion_rate_report.rb
+++ app/reports/spree/payment_method_transactions_conversion_rate_report.rb
@@ -1,9 +1,13 @@
 module Spree
   class PaymentMethodTransactionsConversionRateReport < Spree::Report
     DEFAULT_SORTABLE_ATTRIBUTE = :payment_method_name
-    HEADERS = { payment_method_name: :string, successful_payments_count: :integer, failed_payments_count: :integer, pending_payments_count: :integer, invalid_payments_count: :integer }
+    HEADERS = { payment_state: :string, months_name: :string, count: :integer }
     SEARCH_ATTRIBUTES = { start_date: :payments_created_from, end_date: :payments_created_to }
     SORTABLE_ATTRIBUTES = [:payment_method_name, :successful_payments_count, :failed_payments_count, :pending_payments_count, :invalid_payments_count] 
+
+    def self.no_pagination?
+      true
+    end
 
     def initialize(options)
       super
@@ -11,21 +15,77 @@ module Spree
     end
 
     def generate
-      SpreeReportify::ReportDb[:spree_payment_methods___payment_methods].
+      payment_methods = SpreeReportify::ReportDb[:spree_payment_methods___payment_methods].
       join(:spree_payments___payments, payment_method_id: :id).
-      where(payments__created_at: @start_date..@end_date). #filter by params
-      group(:payment_method_name).
-      order(sortable_sequel_expression)
+      # where(payments__created_at: @start_date..@end_date). #filter by params
+      select{[
+        payment_method_id,
+        Sequel.as(name, :payment_method_name),
+        Sequel.as(state, :payment_state),
+        Sequel.as(MONTHNAME(:payments__created_at), :month_name),
+        Sequel.as(MONTH(:payments__created_at), :number),
+        Sequel.as(YEAR(:payments__created_at), :year)
+      ]}
+
+      group_by_months = SpreeReportify::ReportDb[payment_methods].
+      group(:months_name, :payment_state).
+      order(:year, :number).
+      select{[
+        number,
+        payment_state,
+        year,
+        Sequel.as(concat(month_name, ' ', year), :months_name),
+        Sequel.as(COUNT(payment_method_id), :count),
+      ]}
+
+      grouped_by_payment_state = group_by_months.all.group_by { |record| record[:payment_state] }
+      data = []
+      grouped_by_payment_state.each_pair do |state, collection|
+        data << fill_missing_values({ payment_state: state, count: 0 }, collection)
+      end
+      @data = data.flatten
+    end
+
+    def group_by_payment_state
+      @grouped_by_payment_state ||= @data.group_by { |record| record[:payment_state] }
+    end
+
+    def chart_data
+      {
+        months_name: group_by_payment_state.first.second.map { |record| record[:months_name] },
+        collection: group_by_payment_state
+      }
+    end
+
+    def chart_json
+      {
+        chart: true,
+        charts: [
+          {
+            id: 'payment-state',
+            json: {
+              chart: { type: 'column' },
+              title: { text: 'Payment State' },
+              xAxis: { categories: chart_data[:months_name] },
+              yAxis: {
+                title: { text: 'Count' }
+              },
+              tooltip: { valuePrefix: '#' },
+              legend: {
+                  layout: 'vertical',
+                  align: 'right',
+                  verticalAlign: 'middle',
+                  borderWidth: 0
+              },
+              series: chart_data[:collection].map { |key, value| { name: key, data: value.map { |r| r[:count].to_i } } }
+            }
+          }
+        ]
+      }
     end
 
     def select_columns(dataset)
-      dataset.select{[
-        payment_methods__name.as(payment_method_name),
-        Sequel.as(sum(Sequel.case({{ state: 'completed' } => 1 }, 0)), :successful_payments_count),
-        Sequel.as(sum(Sequel.case({{ state: 'failure' } => 1 }, 0)), :failure_payments_count),
-        Sequel.as(sum(Sequel.case({{ state: 'invalid' } => 1 }, 0)), :invalid_payments_count),
-        Sequel.as(sum(Sequel.case({{ state: 'pending' } =>1 }, 0)), :pending_payments_count)
-      ]}
+      dataset
     end
   end
 end
